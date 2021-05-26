2dcc71031af7fa280fa579a5d210a3211f6578b9
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-05-25 22:33:08 -0700

    working on comments model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,28 +1,36 @@
 const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
-  },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  size: {
-    type: String,
-    default: 'Large'
-  },
-  toppings: [],
-  comments: [
+const PizzaSchema = new Schema(
     {
-      type: Schema.Types.ObjectId,
-      ref: 'Comment'
+      pizzaName: {
+        type: String
+      },
+      createdBy: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      size: {
+        type: String,
+        default: 'Large'
+      },
+      toppings: [],
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
     }
-  ]
-});
+  );
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
